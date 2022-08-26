cf51661dc2424f04cc7cbe061ef7fd366150b8dd
Bhamdou <qgubb21@gmail.com>
2022-08-26 09:04:48 +0200

    first commit

---
+++ exercise7.py
@@ -0,0 +1,12 @@
+# # lower
+
+# text = "Book or other writTen or printed work, Regarded in terms of its conTent raTher than its phYsical form."
+# text = text.lower()
+# text = (text)
+# print(text)
+
+
+# #upper
+# my_string = "Book or other writTen or printed work, Regarded in terms of its conTent raTher than its phYsical form."
+# print(my_string.upper())
+
